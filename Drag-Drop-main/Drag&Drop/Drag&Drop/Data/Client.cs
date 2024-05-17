dae527210a7a0dbb7d8dc539b46ffba68ea71a53
unknown <alexandarmitov10@gmail.com>
2024-05-17 23:50:01 +0300

    LogInRework

---
+++ Drag-Drop-main/Drag&Drop/Drag&Drop/Data/Client.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Drag_Drop.Data
+{
+    public class Client:IdentityUser
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime RegisterOn { get; set; } = DateTime.Now;
+        public ICollection<Order> Orders { get; set; }
+    }
+}
