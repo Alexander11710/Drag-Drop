326c39410b8db3e3c7f224d67549aada60ae8787
unknown <alexandarmitov10@gmail.com>
2024-05-18 00:02:30 +0300

    Design

--- Drag-Drop-main/Drag-Drop-main/Drag&Drop/Drag&Drop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Drag_Drop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
